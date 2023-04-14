1d036f5993ff07d69dd83cc0b8743b8031025132
Mayank Raj <mayankrj1224@gmail.com>
2023-04-14 18:35:11 +0530

    small bug fixed

--- src/Pages/FoodForm.jsx
+++ src/Pages/FoodForm.jsx
@@ -37,51 +37,70 @@ const FoodForm = () => {
     };
 
     return (
-        <Box style={{ margin : 'auto',justifyContent:'center' , border:'2px solid red'}}>
-            <form onSubmit={handleSubmit} style={{ width: '50%', marginTop: '70px' }}>
-                <TextField
-                    id="food-name"
-                    label="Food Name"
-                    variant="outlined"
-                    value={foodName}
-                    onChange={handleFoodNameChange}
-                    margin="normal"
-                    required
-                    fullWidth
-                />
-                <TextField
-                    id="food-type"
-                    select
-                    label="Food Type"
-                    variant="outlined"
-                    value={foodType}
-                    onChange={handleFoodTypeChange}
-                    margin="normal"
-                    required
-                    fullWidth
-                >
-                    <MenuItem value="Delicious Food">Delicious Food</MenuItem>
-                    <MenuItem value="Nutritious Food">Nutritious Food</MenuItem>
-                    <MenuItem value="Fast Food">Fast Food</MenuItem>
-                    <MenuItem value="Beverages">Beverages</MenuItem>
-                    <MenuItem value="Desserts">Desserts</MenuItem>
-                </TextField>
-                <TextField
-                    id="max-delivery-time"
-                    label="Max Delivery Time (in minutes)"
-                    type="number"
-                    variant="outlined"
-                    value={maxDeliveryTime}
-                    onChange={handleMaxDeliveryTimeChange}
-                    margin="normal"
-                    required
-                    fullWidth
-                />
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                    Submit
-                </Button>
-            </form>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundColor: 'lightblue',
+            }}
+        >
+            <Box
+                sx={{
+                    width: '50%',
+                    padding: '20px',
+                    backgroundColor: 'white',
+                    borderRadius: '10px',
+                    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                }}
+            >
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        id="food-name"
+                        label="Food Name"
+                        variant="outlined"
+                        value={foodName}
+                        onChange={handleFoodNameChange}
+                        margin="normal"
+                        required
+                        fullWidth
+                    />
+                    <TextField
+                        id="food-type"
+                        select
+                        label="Food Type"
+                        variant="outlined"
+                        value={foodType}
+                        onChange={handleFoodTypeChange}
+                        margin="normal"
+                        required
+                        fullWidth
+                    >
+                        <MenuItem value="Delicious Food">Delicious Food</MenuItem>
+                        <MenuItem value="Nutritious Food">Nutritious Food</MenuItem>
+                        <MenuItem value="Fast Food">Fast Food</MenuItem>
+                        <MenuItem value="Beverages">Beverages</MenuItem>
+                        <MenuItem value="Desserts">Desserts</MenuItem>
+                    </TextField>
+                    <TextField
+                        id="max-delivery-time"
+                        label="Max Delivery Time (in minutes)"
+                        type="number"
+                        variant="outlined"
+                        value={maxDeliveryTime}
+                        onChange={handleMaxDeliveryTimeChange}
+                        margin="normal"
+                        required
+                        fullWidth
+                    />
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Submit
+                    </Button>
+                </form>
+            </Box>
         </Box>
+
     );
 };
 
